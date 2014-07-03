8c5eebea83206cbf595bfa70aef6b419c43467de
Serge <sly_fox4@mail.ru>
2014-07-03 20:09:15 +0400

    Class created

--- connection.cpp
+++ connection.cpp
@@ -1,18 +1,10 @@
 #include "header.h"
 
-message mes;
-SOCKET Listen;
-SOCKET Connect;
-
-void StartWinSock()
+Server::Server()
 {
-	cout << "Server: " << endl;
-	WSAData wsa;
 	WORD Version = MAKEWORD(2, 1);
-
 	WSAStartup(Version, &wsa);
-
-	if(WSAStartup(Version, &wsa) != 0)
+    if(WSAStartup(Version, &wsa) != 0)
 	{
 		cout << "WSA library startup error." << endl;
 	}
@@ -22,38 +14,91 @@ void StartWinSock()
 	}
 	Listen = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	Connect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-}
 
-
-
-void CreateSock()
-{
 	if(Listen == SOCKET_ERROR || Connect == SOCKET_ERROR)
 	{
 		cout << "Socket error. Can't create socket." << endl;
-    }
-    else
+	}
+	else
 	{
 		cout << "Socket create complite." << endl;
 	}
-	SOCKADDR_IN Server;
 
-    Server.sin_family = AF_INET;
+	Server.sin_family = AF_INET;
 	Server.sin_port = htons(1337);
 	Server.sin_addr.s_addr = INADDR_ANY; // !!!
 
 	if( bind(Listen, (SOCKADDR*)&Server, sizeof(Server)) == SOCKET_ERROR)
 	{
 		cout << "Can't bind socket." << endl;
-    }
-    else
+	}
+	else
 	{
 		cout << "Socket bind complite." << endl;
 	}
 	listen(Listen, SOMAXCONN);
+
+	airList = new Airport();
 }
 
-void Waiting()
+
+//
+//
+//
+message mes;
+//SOCKET Listen;
+//SOCKET Connect;
+//
+//void StartWinSock()
+//{
+//	cout << "Server: " << endl;
+//	WSAData wsa;
+//	WORD Version = MAKEWORD(2, 1);
+//
+//	WSAStartup(Version, &wsa);
+//
+//	if(WSAStartup(Version, &wsa) != 0)
+//	{
+//		cout << "WSA library startup error." << endl;
+//	}
+//	else
+//	{
+//		cout << "WSA library startup complite." << endl;
+//	}
+//	Listen = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//	Connect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//}
+//
+//
+//
+//void CreateSock()
+//{
+//	if(Listen == SOCKET_ERROR || Connect == SOCKET_ERROR)
+//	{
+//		cout << "Socket error. Can't create socket." << endl;
+//	}
+//	else
+//	{
+//		cout << "Socket create complite." << endl;
+//	}
+//	SOCKADDR_IN Server;
+//
+//	Server.sin_family = AF_INET;
+//	Server.sin_port = htons(1337);
+//	Server.sin_addr.s_addr = INADDR_ANY; // !!!
+//
+//	if( bind(Listen, (SOCKADDR*)&Server, sizeof(Server)) == SOCKET_ERROR)
+//	{
+//		cout << "Can't bind socket." << endl;
+//	}
+//	else
+//	{
+//		cout << "Socket bind complite." << endl;
+//	}
+//	listen(Listen, SOMAXCONN);
+//}
+
+void Server::startListening()
 {
 	while(true)
 	{
@@ -66,8 +111,25 @@ void Waiting()
 			break;
 		}
 		Sleep(100);
-
 	}
-
-
 }
+
+
+//void Waiting()
+//{
+//	while(true)
+//	{
+//		if(Connect = accept(Listen, NULL, NULL))
+//		{
+//			cout << "Client complite connected." << endl;
+//			recv(Connect, (char *)&mes, sizeof(mes), 0);
+//			//send(Connect, "SENDING!?", sizeof(string), 0);
+//			cout<< mes.ID;
+//			break;
+//		}
+//		Sleep(100);
+//
+//	}
+//
+//
+//}
--- header.h
+++ header.h
@@ -7,6 +7,9 @@
 #include <windows.h>
 #include <winsock2.h>
 #include <System.hpp>
+#include <vector>
+
+#define numOfAir 10
 
 using namespace std;
 
@@ -14,6 +17,43 @@ void StartWinSock();
 void CreateSock();
 void Waiting();
 
+struct flight
+{
+	char idFrom[3];
+	char nameFrom[20];
+	char idTo[3];
+	char nameTo[20];
+	bool typeOfFlight;
+	TDateTime date;
+	flight* next;
+};
+
+class Airport
+{
+	private:
+		char id[3];
+		char name[20];
+		flight firstFlight;
+	public:
+		Airport();
+		void getAirport();
+};
+
+class Server
+{
+	private:
+		WSAData wsa;
+		SOCKET Listen;
+		SOCKET Connect;
+		SOCKADDR_IN Server;
+	public:
+		Server();
+		void startListening();
+		void sendAirList();
+		void sendFlightList();
+		void getFlightList();
+		vector<Airport> airList;
+};
 
 
 struct message
@@ -21,4 +61,3 @@ struct message
 	int ID;
 	string time;
 };
-
