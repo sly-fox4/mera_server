4aafb765c0c596f7cbb70ec27f43a08913d75814
Serge <sly_fox4@mail.ru>
2014-07-03 20:09:50 +0400

    delete some comments

--- connection.cpp
+++ connection.cpp
@@ -41,62 +41,7 @@ Server::Server()
 	airList = new Airport();
 }
 
-
-//
-//
-//
 message mes;
-//SOCKET Listen;
-//SOCKET Connect;
-//
-//void StartWinSock()
-//{
-//	cout << "Server: " << endl;
-//	WSAData wsa;
-//	WORD Version = MAKEWORD(2, 1);
-//
-//	WSAStartup(Version, &wsa);
-//
-//	if(WSAStartup(Version, &wsa) != 0)
-//	{
-//		cout << "WSA library startup error." << endl;
-//	}
-//	else
-//	{
-//		cout << "WSA library startup complite." << endl;
-//	}
-//	Listen = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-//	Connect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-//}
-//
-//
-//
-//void CreateSock()
-//{
-//	if(Listen == SOCKET_ERROR || Connect == SOCKET_ERROR)
-//	{
-//		cout << "Socket error. Can't create socket." << endl;
-//	}
-//	else
-//	{
-//		cout << "Socket create complite." << endl;
-//	}
-//	SOCKADDR_IN Server;
-//
-//	Server.sin_family = AF_INET;
-//	Server.sin_port = htons(1337);
-//	Server.sin_addr.s_addr = INADDR_ANY; // !!!
-//
-//	if( bind(Listen, (SOCKADDR*)&Server, sizeof(Server)) == SOCKET_ERROR)
-//	{
-//		cout << "Can't bind socket." << endl;
-//	}
-//	else
-//	{
-//		cout << "Socket bind complite." << endl;
-//	}
-//	listen(Listen, SOMAXCONN);
-//}
 
 void Server::startListening()
 {
@@ -114,22 +59,3 @@ void Server::startListening()
 	}
 }
 
-
-//void Waiting()
-//{
-//	while(true)
-//	{
-//		if(Connect = accept(Listen, NULL, NULL))
-//		{
-//			cout << "Client complite connected." << endl;
-//			recv(Connect, (char *)&mes, sizeof(mes), 0);
-//			//send(Connect, "SENDING!?", sizeof(string), 0);
-//			cout<< mes.ID;
-//			break;
-//		}
-//		Sleep(100);
-//
-//	}
-//
-//
-//}
